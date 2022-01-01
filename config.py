6ce750233c939507181ccacf413dce3d81537bda
noahzhy <noahzhy@gmail.com>
2022-01-01 12:03:27 +0900

    double 9380 mAP 9630

--- config.py
+++ config.py
@@ -6,12 +6,12 @@ CHANNEL = 1
 SEED = 1234
 N_SAMPLE = 1000
 # training config
-BATCH_SIZE = 128
-TRAIN_SAMPLE = 7472
-NUM_EPOCHS = 500
-WARMUP_EPOCH = 20
-LEARNING_RATE = 1e-3
-# LEARNING_RATE = 3e-4
+BATCH_SIZE = 256
+TRAIN_SAMPLE = 112450
+NUM_EPOCHS = 200
+WARMUP_EPOCH = 10
+# LEARNING_RATE = 1e-3
+LEARNING_RATE = 3e-4
 
 CHARS = "0123456789가나다라마거너더러머버서어저고노도로모보소오조구누두루무부수우주하허호바사아자배abcdefghijklmnopqABCDEFGHIJKLMNOPQ"  # exclude IO
 CHARS_DICT = {char: i for i, char in enumerate(CHARS)}
@@ -21,9 +21,9 @@ DECODE_DICT = {i: char for i, char in enumerate(CHARS)}
 NUM_CLASS = len(CHARS)+1
 LABEL_MAX_LEN = 8
 
-# TRAIN_DIR = r'C:\dataset\license_plate\license_plate_recognition\train'
-# VAL_DIR = r'C:\dataset\license_plate\license_plate_recognition\val'
-TRAIN_DIR = r'C:\dataset\license_plate\license_plate_recognition\double\train'
-VAL_DIR = r'C:\dataset\license_plate\license_plate_recognition\double\val'
+TRAIN_DIR = r'C:\dataset\license_plate\license_plate_recognition\train'
+VAL_DIR = r'C:\dataset\license_plate\license_plate_recognition\val'
+# TRAIN_DIR = r'C:\dataset\license_plate\license_plate_recognition\double\train'
+# VAL_DIR = r'C:\dataset\license_plate\license_plate_recognition\double\val'
 TEST_DIR = VAL_DIR
-# TEST_DIR = r'C:\dataset\license_plate\license_plate_recognition\double'
+# TEST_DIR = r'C:\dataset\license_plate\license_plate_recognition\double\val'
